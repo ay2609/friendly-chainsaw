a886ec5f06df27e66ffb4d0ebbda775adbf91692
TheGreenBandana <kyle.c.benjamin@icloud.com>
2024-01-02 18:48:47 -0500

    added beatles, adjusted main for it

--- __main__.py
+++ __main__.py
@@ -13,13 +13,23 @@ print(database.list_songs())
 
 # Replace with your own songs
 
-songs = [r"D:\My Music\Pink Floyd Wish You Were Here.mp3",
-         r"D:\My Music\Beatles_TwistAndShout.mp3",
-         r"D:\My Music\JimiHendrix_LittleWing.mp3"]
+songs = [
+    #r"D:\My Music\Pink Floyd Wish You Were Here.mp3",
+    "Beatles_TwistAndShout.mp3"
+    #, r"D:\My Music\JimiHendrix_LittleWing.mp3"
+    ]
 
-names = ["Wish you were Here", "Twist and Shout", "Little Wing"]
+names = [
+    #"Wish you were Here",
+    "Twist and Shout"
+    #, "Little Wing"
+    ]
 
-artists = ["Pink Floyd", "The Beatles", "Jimi Hendrix"]
+artists = [
+    #"Pink Floyd",
+    "The Beatles"
+    #, "Jimi Hendrix"
+    ]
 
 start = time()
 #db.add_songs(songs, names, artists)
@@ -28,3 +38,4 @@ dt = time() - start
 
 
 database.save()
+
