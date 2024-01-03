e4d834dec3b417512b817b4b3308b0b57c4dc847
literallyanything <hunterbaker@me.com>
2024-01-03 14:16:35 -0500

    Add rich_repr

--- garbanzo_reader.py
+++ garbanzo_reader.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Iterator
+from typing import Iterator, Generator, Any
 import pathlib
 
 _garbanzo_path = os.path.join(pathlib.Path(__file__).parent.resolve(), 'sturdy-garbanzo')
@@ -12,6 +12,11 @@ class Garbanzo:
         self.filename = os.path.join(_garbanzo_path, data['file'])
         self.title = data.get('title', 'Kyle forgot the title')
         self.artist = data.get('artist', 'Kyle forgot the artist')
+
+    def __rich_repr__(self) -> Generator[Any, Any, Any]:
+        yield self.filename
+        yield 'title', self.title
+        yield 'artist', self.artist
 
     def __repr__(self) -> str:
         return f"<Garbanzo filename={self.filename}, title={self.title} artist={self.artist}>"
