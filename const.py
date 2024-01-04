14cb40a9c3dabb93c5ae644e21067503af361c34
ay2609 <ayadava@ideaventionsacademy.org>
2024-01-04 09:23:17 -0500

    adjust const values to have recognizable peaks

--- const.py
+++ const.py
@@ -10,11 +10,11 @@ SAMPLING_RATE: int = 44100
 # The fractional portion of intensities for which the cutoff is selected.
 # E.g. frac_cut=0.8 will produce a cutoff intensity such that the bottom 80%
 # of intensities are excluded.
-MIN_FRAC_AMP_CUTOFF: float = 0.80
+MIN_FRAC_AMP_CUTOFF: float = 0.005
 
 # The neighborhood radius used for determining if a spectrogram value
 # is a local peak. Specified in spectrogram cells.
-LOCAL_PEAK_NN_RADIUS: int = 20
+LOCAL_PEAK_NN_RADIUS: int = 10
 
 # Given a spectrogram peak, indicates the maximum number of subsequent peaks to
 # be used to form fingerprint features.
